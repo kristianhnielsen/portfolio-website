7130ee4f38f51a7cdc81ed79864549b9810362f4
Kristian <kristianhnielsen@protonmail.com>
2023-12-12 21:14:55 +0800

    add theme for blog post

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,6 +19,24 @@ module.exports = {
           "100%": { opacity: "0", scale: "0" },
         },
       },
+      typography: ({ theme }) => ({
+        "cold-slate": {
+          css: {
+            "--tw-prose-body": theme("colors.slate[400]"),
+            "--tw-prose-headings": theme("colors.slate[200]"),
+            "--tw-prose-lead": theme("colors.slate[400]"),
+            "--tw-prose-links": theme("colors.emerald[600]"),
+            "--tw-prose-bold": theme("colors.slate[400]"),
+            "--tw-prose-counters": theme("colors.slate[400]"),
+            "--tw-prose-bullets": theme("colors.slate[400]"),
+            "--tw-prose-hr": theme("colors.slate[400]"),
+            "--tw-prose-quotes": theme("colors.slate[400]"),
+            "--tw-prose-quote-borders": theme("colors.slate[400]"),
+            "--tw-prose-captions": theme("colors.slate[400]"),
+            "--tw-prose-code": theme("colors.slate[400]"),
+          },
+        },
+      }),
     },
   },
   plugins: [
